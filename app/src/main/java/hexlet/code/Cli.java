1b6da9d7a727259b5c7391d3f4e15df1bfe853a7
Oleg Isanbaev <ingen.ole@yandex.ru>
2023-07-06 01:11:29 +0300

    Fix linter errors

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greetings () {
+    public static void greetings() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name?");
 
