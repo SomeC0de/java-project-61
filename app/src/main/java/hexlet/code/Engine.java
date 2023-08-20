714215cf674d8a2c042594e83fb5de19cd6a02f1
Oleg Isanbaev <ingen.ole@yandex.ru>
2023-08-21 01:18:15 +0300

    WIP: implement array of interfaces

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,11 +1,30 @@
 package hexlet.code;
+import hexlet.code.games.Even;
+import hexlet.code.games.GameInterface;
 import hexlet.code.games.GamesList;
+
+import javax.lang.model.type.NullType;
+import java.util.Scanner;
 
 public class Engine {
     GamesList actualGame;
     final static int ATTEMPTS_COUNT = 3;
+    Scanner inputScan;
+    Even gameEven = new Even();
+    GameInterface[] arrayGamesMethods = {null, null, gameEven};
 
-    public void runEngine() {
+    public void runEngine(int gameId) {
+        switch (gameId) {
+            case 1:
+                actualGame = GamesList.EVEN;
+                break;
 
+            case 2:
+                actualGame = GamesList.CALC;
+                break;
+
+            default:
+                break;
+        }
     }
 }
