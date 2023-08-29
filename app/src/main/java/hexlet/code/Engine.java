6424be95669a6c54781bf267f7f97bf33b890ca6
Oleg Isanbaev <ingen.ole@yandex.ru>
2023-08-29 22:41:37 +0300

    Fix checkstyle errors

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -9,19 +9,19 @@ import hexlet.code.games.Gcd;
 import hexlet.code.games.Progression;
 
 import hexlet.code.games.Prime;
-public class Engine {
+public final class Engine {
     private static final int TRIAL_MAX_CNT = 3;
     public static final int ID_GAME_EVEN = 2;
     public static final int ID_GAME_CALC = 3;
     public static final int ID_GAME_GCD = 4;
     public static final int ID_GAME_PROGRESSION = 5;
     public static final int ID_GAME_PRIME = 6;
-    private Even gameEven = new Even();
-    private Calc gameCalc = new Calc();
-    private Gcd gameGcd = new Gcd();
-    private Progression gameProgression = new Progression();
-    private Prime gamePrime = new Prime();
-    private GameInterface[] arrayGamesMethods = {null, null, gameEven, gameCalc, gameGcd, gameProgression, gamePrime};
+    private final Even gameEven = new Even();
+    private final Calc gameCalc = new Calc();
+    private final Gcd gameGcd = new Gcd();
+    private final Progression gameProgression = new Progression();
+    private final Prime gamePrime = new Prime();
+    private final GameInterface[] GAMES_LIST = {null, null, gameEven, gameCalc, gameGcd, gameProgression, gamePrime};
 
     public void runEngine(int idGame) {
         int trialNum;
@@ -32,20 +32,20 @@ public class Engine {
             case ID_GAME_GCD:
             case ID_GAME_PROGRESSION:
             case ID_GAME_PRIME:
-                arrayGamesMethods[idGame].initGame();
-                arrayGamesMethods[idGame].sayHello();
-                arrayGamesMethods[idGame].explainRules();
+                GAMES_LIST[idGame].initGame();
+                GAMES_LIST[idGame].sayHello();
+                GAMES_LIST[idGame].explainRules();
 
                 for (trialNum = 0; trialNum < TRIAL_MAX_CNT; trialNum++) {
-                    arrayGamesMethods[idGame].generateQuestion();
-                    arrayGamesMethods[idGame].getAnswer();
+                    GAMES_LIST[idGame].generateQuestion();
+                    GAMES_LIST[idGame].getAnswer();
 
-                    if (!arrayGamesMethods[idGame].isAnswerCorrect()) {
+                    if (!GAMES_LIST[idGame].isAnswerCorrect()) {
                         break;
                     }
                 }
 
-                arrayGamesMethods[idGame].sayGoodbye(trialNum, TRIAL_MAX_CNT);
+                GAMES_LIST[idGame].sayGoodbye(trialNum, TRIAL_MAX_CNT);
                 break;
 
             default:
