582b62fc88f6da03bb8737ebb7f7f9835c62d7a3
Oleg Isanbaev <ingen.ole@yandex.ru>
2023-09-09 02:13:15 +0300

    GameInterface: refactor const modifs

--- app/src/main/java/hexlet/code/GameInterface.java
+++ app/src/main/java/hexlet/code/GameInterface.java
@@ -1,9 +1,9 @@
 package hexlet.code;
 
 public interface GameInterface {
-    public static final int GAME_DATA_QUEST_IDX = 0;
-    public static final int GAME_DATA_ANSW_IDX = 1;
-    public static final int GAME_DATA_LEN = 2;
+    int GAME_DATA_QUEST_IDX = 0;
+    int GAME_DATA_ANSW_IDX = 1;
+    int GAME_DATA_LEN = 2;
     String getRules();
     String[] getGameData();
 }
